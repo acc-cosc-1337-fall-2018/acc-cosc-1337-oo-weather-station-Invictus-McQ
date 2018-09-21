39ccb6c91d3fb5007aadf9151186460ef6650d13
Kevin McCue Home <mcckm.kvn@gmail.com>
2018-09-20 19:19:41 -0500

    first work

--- test/model/clock_test.cpp
+++ test/model/clock_test.cpp
@@ -1,2 +1,25 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "clock.h"
+
+TEST_CASE("Test get hours from seconds")
+{
+	Clock clock;
+	REQUIRE(clock.get_hours(3600) == 1);
+	REQUIRE(clock.get_hours(34950) == 9);
+}
+TEST_CASE("Test get minutes from seconds")
+{
+	Clock clock;
+	REQUIRE(clock.get_minutes(3600) == 0);
+	REQUIRE(clock.get_minutes(3800) == 3);
+	REQUIRE(clock.get_minutes(68950) == 42);
+}
+
+TEST_CASE("Test get seconds from seconds")
+{
+	Clock clock;
+	REQUIRE(clock.get_seconds(3600) == 0);
+	REQUIRE(clock.get_seconds(38000) == 20);
+	REQUIRE(clock.get_seconds(68950) == 42);
+}
